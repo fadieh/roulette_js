c8b290b0749668b78f036348ead6115b8495e1a3
Fadie Hannona <fadiehannona@gmail.com>
2014-11-04 19:04:59 +0000

    Has the colour type black

--- spec/roulette_spec.js
+++ spec/roulette_spec.js
@@ -8,8 +8,14 @@ it ('will have a range of numbers', function(){
   expect(roulette.numbers.length).toEqual(37)
 });
 
-it ('will have a colour type', function(){
+it ('will have a colour type red', function(){
+  roulette.setColourToRed()
   expect(roulette.colour).toEqual("red")
+});
+
+it ('will have a colour type black', function(){
+  roulette.setColourToBlack()
+  expect(roulette.colour).toEqual("black")
 });
 
 });
