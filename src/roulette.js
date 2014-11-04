c92104c85c540647143d17b24a9486c69f65af23
Fadie Hannona <fadiehannona@gmail.com>
2014-11-04 19:37:58 +0000

    All numbers have a colour

--- src/roulette.js
+++ src/roulette.js
@@ -5,6 +5,17 @@ function Roulette() {
   this.third = ''
 };
 
+// SETTING COLOURS TO NUMBERS
+
+Roulette.prototype.setColourToNumbers = function() {
+  if (this.number == 2 || this.number == 4 || this.number == 6 || this.number == 8 || this.number == 10 || this.number == 11 || this.number == 13 || this.number == 15 || this.number == 17 || this.number == 20 || this.number == 22 || this.number == 24 || this.number == 26 || this.number == 29 || this.number == 28 || this.number == 29 || this.number == 31 || this.number == 33 || this.number == 35)
+  { this.colour = 'black' }
+  else if (this.number == 1 || this.number == 3 || this.number == 5 || this.number == 7 || this.number == 9 || this.number == 12 || this.number == 14 || this.number == 16 || this.number == 18 || this.number == 19 || this.number == 21 || this.number == 23 || this.number == 25 || this.number == 27 || this.number == 30 || this.number == 32 || this.number == 34 || this.number == 36)
+  { this.colour = 'red'}
+  else
+  { this.colour = null}
+};
+
 Roulette.prototype.setColourToBlack = function() {
   this.colour = "black"
 };
@@ -13,14 +24,7 @@ Roulette.prototype.setColourToRed = function() {
   this.colour = "red"
 };
 
-Roulette.prototype.setColourToNumbers = function() {
-  if (this.number == 0)
-  { this.colour = null }
-  else if (this.number == 1)
-  { this.colour = 'red'}
-  else
-  { this.colour = 'black'}
-};
+// SETTING THIRDS TO NUMBERS
 
 Roulette.prototype.setThirdToNumber = function () {
   if (this.number < 13)
