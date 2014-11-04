315b2529f02436da4d7f9aab39eb2bb4d14086b4
Fadie Hannona <fadiehannona@gmail.com>
2014-11-04 19:00:30 +0000

    Has the colour type red

--- spec/roulette_spec.js
+++ spec/roulette_spec.js
@@ -8,4 +8,8 @@ it ('will have a range of numbers', function(){
   expect(roulette.numbers.length).toEqual(37)
 });
 
+it ('will have a colour type', function(){
+  expect(roulette.colour).toEqual("red")
+});
+
 });
